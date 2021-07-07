5bd314905dacbd84777ba34bd111956187233ba8
Aline Milene <alinem2104@gmail.com>
2021-07-07 14:47:59 -0300

    Exercicio de conta com exceptions

---
+++ ExercicioExceptions/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioExceptions.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
+
---
+++ ExercicioExceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using ExercicioExceptions.Entities;
+using ExercicioExceptions.Entities.Exceptions;
+
+namespace ExercicioExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account conta = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                conta.Withdraw(amount);
+                Console.WriteLine("New balance: " + conta.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
